3d9f7347afb150bf96d9480ed5b179f8d49c9c92
SANGHYUN0519 <tkdgus0519@naver.com>
2024-11-03 16:02:30 +0900

    [chore] : #27 DTO 어노테이션 수정

--- article1_be/article1-be/src/main/java/article1be/review/dto/ReviewDTO.java
+++ article1_be/article1-be/src/main/java/article1be/review/dto/ReviewDTO.java
@@ -1,14 +1,12 @@
 package article1be.review.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ReviewDTO {
 
     private Long reviewSeq;         // 리뷰 번호
