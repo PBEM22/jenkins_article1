3598220a6b4b73206eb56739f3daa8f561b4f58f
SANGHYUN0519 <tkdgus0519@naver.com>
2024-11-03 16:02:52 +0900

    [chore] : #27 레퍼지토리 수정

--- article1_be/article1-be/src/main/java/article1be/review/repository/ReviewRepository.java
+++ article1_be/article1-be/src/main/java/article1be/review/repository/ReviewRepository.java
@@ -1,12 +1,22 @@
 package article1be.review.repository;
 
-import article1be.common.aggregate.entity.Review;
+
+import article1be.review.aggregate.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // 특정 유저가 작성한 리뷰 조회
+    List<Review> findByUserSeq(Long userSeq);
+
+    // 특정 선택 기록(selectSeq)에 대한 리뷰 조회
+    List<Review> findBySelectSeq(Long selectSeq);
+
 
 }
