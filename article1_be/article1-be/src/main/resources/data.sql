aac968254413822358a321d7a11a78acc47a9b03
person76 <94957124+person76@users.noreply.github.com>
2024-11-03 19:18:20 +0900

    [CHORE] DDL, DML 구문 수정
    DDL 구문 대문자->소문자 수정 및 DML 구문 추가

---
+++ article1_be/article1-be/src/main/resources/data.sql
@@ -0,0 +1,184 @@
+INSERT INTO outfit (outfit_name, outfit_weather, outfit_temp_max, outfit_temp_min, outfit_category, outfit_gender, outfit_level, outfit_img) VALUES
+-- 상의
+('맨투맨', NULL, 20, -100, 'TOP', 'N', NULL, 'example.com/image1.jpg'),
+('반팔_티셔츠', NULL, 100, 20, 'TOP', 'N', NULL, 'example.com/image2.jpg'),
+('셔츠', NULL, 25, 15, 'TOP', 'N', NULL, 'example.com/image3.jpg'),
+('반팔셔츠', NULL, 20, 100, 'TOP', 'N', NULL, 'example.com/image4.jpg'),
+('린넨셔츠', NULL, 20, 100, 'TOP', 'N', NULL, 'example.com/image5.jpg'),
+('니트_스웨터', NULL, 15, -100, 'TOP', 'N', NULL, 'example.com/image6.jpg'),
+('후드티', NULL, 18, 8, 'TOP', 'N', NULL, 'example.com/image7.jpg'),
+('폴로_반팔티', NULL, 28, 18, 'TOP', 'M', NULL, 'example.com/image8.jpg'),
+('나시', NULL, 35, 25, 'TOP', 'N', NULL, 'example.com/image9.jpg'),
+('터틀넥_스웨터', NULL, 12, 0, 'TOP', 'N', NULL, 'example.com/image10.jpg'),
+('블라우스', NULL, 25, 15, 'TOP', 'F', NULL, 'example.com/image11.jpg'),
+
+-- 하의
+('긴청바지', NULL, 25, 10, 'BOTTOM', 'N', NULL, 'example.com/image12.jpg'),
+('긴면바지', NULL, 30, 15, 'BOTTOM', 'M', NULL, 'example.com/image13.jpg'),
+('반바지', NULL, 35, 20, 'BOTTOM', 'N', NULL, 'example.com/image14.jpg'),
+('슬랙스', NULL, 28, 18, 'BOTTOM', 'N', NULL, 'example.com/image15.jpg'),
+('트레이닝_팬츠', NULL, 20, 5, 'BOTTOM', 'N', NULL, 'example.com/image16.jpg'),
+('조거_팬츠', NULL, 18, 8, 'BOTTOM', 'N', NULL, 'example.com/image17.jpg'),
+('와이드_팬츠', NULL, 25, 15, 'BOTTOM', 'N', NULL, 'example.com/image18.jpg'),
+('스키니_진', NULL, 22, 12, 'BOTTOM', 'F', NULL, 'example.com/image19.jpg'),
+('스커트', NULL, 30, 20, 'BOTTOM', 'F', NULL, 'example.com/image20.jpg'),
+('롱_스커트', NULL, 28, 18, 'BOTTOM', 'F', NULL, 'example.com/image21.jpg'),
+
+-- 아우터
+('자켓', NULL, 15, 5, 'OUTERWEAR', 'N', NULL, 'example.com/image22.jpg'),
+('코트', NULL, 15, -10, 'OUTERWEAR', 'N', NULL, 'example.com/image23.jpg'),
+('패딩', NULL, 10, -100, 'OUTERWEAR', 'N', NULL, 'example.com/image24.jpg'),
+('후드_집업', NULL, 20, 10, 'OUTERWEAR', 'N', NULL, 'example.com/image25.jpg'),
+('트렌치코트', NULL, 20, 8, 'OUTERWEAR', 'N', NULL, 'example.com/image26.jpg'),
+('무스탕', NULL, 12, -3, 'OUTERWEAR', 'N', NULL, 'example.com/image27.jpg'),
+('경량_패딩', NULL, 15, 0, 'OUTERWEAR', 'N', NULL, 'example.com/image28.jpg'),
+('바람막이', NULL, 20, 10, 'OUTERWEAR', 'N', NULL, 'example.com/image29.jpg'),
+('얇은_가디건', NULL, 25, 15, 'OUTERWEAR', 'N', NULL, 'example.com/image30.jpg'),
+
+-- 신발
+('운동화', NULL, 30, -10, 'SHOES', 'N', NULL, 'example.com/image31.jpg'),
+('슬리퍼', NULL, 100, 20, 'SHOES', 'N', NULL, 'example.com/image32.jpg'),
+('부츠', NULL, 15, -10, 'SHOES', 'N', NULL, 'example.com/image33.jpg'),
+('샌들', NULL, 100, 20, 'SHOES', 'N', NULL, 'example.com/image34.jpg'),
+('로퍼', NULL, 25, -5, 'SHOES', 'N', NULL, 'example.com/image35.jpg'),
+('워커', NULL, 25, -5, 'SHOES', 'N', NULL, 'example.com/image36.jpg'),
+('플랫슈즈', NULL, 30, 0, 'SHOES', 'F', NULL, 'example.com/image37.jpg'),
+('하이힐', NULL, 30, 0, 'SHOES', 'F', NULL, 'example.com/image38.jpg'),
+('발목_부츠', NULL, 20, -20, 'SHOES', 'N', NULL, 'example.com/image39.jpg'),
+('스니커즈', NULL, 30, -10, 'SHOES', 'N', NULL, 'example.com/image40.jpg'),
+('크록스', NULL, 100, 20, 'SHOES', 'N', NULL, 'example.com/image41.jpg'),
+
+-- 악세사리
+('선글라스', 800, 100, 15, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image42.jpg'),
+('넥워머', 801, 10, -100, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image43.jpg'),
+('귀마개', 801, 10, -100, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image44.jpg'),
+('썬캡', 800, 35, 20, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image45.jpg'),
+('장갑', 801, 5, -100, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image46.jpg'),
+('귀도리', 801, 10, -5, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image47.jpg'),
+('양산', 801, 100, 20, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image48.jpg'),
+('목도리', 804, 10, -100, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image49.jpg'),
+('플리스_모자', 801, 10, -10, 'ACCESSORY', 'N', 'REQUIRED', 'example.com/image50.jpg'),
+('자외선_차단제', 800, 100, 15, 'ACCESSORY', 'N', 'REQUIRED', 'example.com/image51.jpg'),
+('휴대용_선풍기', 800, 100, 20, 'ACCESSORY', 'N', 'SELECTION', 'example.com/image52.jpg'),
+('핫팩', 801, 5, -100, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image53.jpg'),
+('우산', 500, 100, -100, 'ACCESSORY', 'N', 'REQUIRED', 'example.com/image54.jpg'),
+('마스크', 804, 100, -100, 'ACCESSORY', 'N', 'REQUIRED', 'example.com/image55.jpg'),
+('비니', 800, 10, -100, 'ACCESSORY', 'N', 'RECOMMENDED', 'example.com/image56.jpg'),
+('손목시계', 800, 100, -100, 'ACCESSORY', 'N', 'SELECTION', 'example.com/image57.jpg'),
+('보조_배터리', 800, 100, -100, 'ACCESSORY', 'N', 'SELECTION', 'example.com/image58.jpg'),
+('손수건', 800, 100, -100, 'ACCESSORY', 'N', 'SELECTION', 'example.com/image59.jpg');
+
+INSERT INTO style (style_name) VALUES
+                                   ('casual'),   -- 1
+                                   ('formal'),   -- 2
+                                   ('sporty');   -- 3
+
+INSERT INTO outfit_style (outfit_seq, style_seq) VALUES
+-- 캐주얼 스타일
+(1, 1),    -- 맨투맨
+(2, 1),    -- 반팔 티셔츠
+(6, 1),    -- 폴로 반팔티
+(7, 1),    -- 나시
+(10, 1),   -- 터틀넥 스웨터
+(12, 1),   -- 긴청바지
+(13, 1),   -- 긴면바지
+(14, 1),   -- 반바지
+(16, 1),   -- 트레이닝 팬츠
+(24, 1),   -- 후드 집업
+(28, 1),   -- 바람막이
+(31, 1),   -- 운동화
+(32, 1),   -- 슬리퍼
+(39, 1),   -- 스니커즈
+(40, 1),   -- 크록스
+
+-- 포멀 스타일
+(3, 2),    -- 셔츠
+(5, 2),    -- 반팔셔츠
+(8, 2),    -- 터틀넥 스웨터
+(15, 2),   -- 슬랙스
+(21, 2),   -- 자켓
+(22, 2),   -- 코트
+(26, 2),   -- 트렌치코트
+(35, 2),   -- 로퍼
+(37, 2),   -- 플랫슈즈
+(38, 2),   -- 하이힐
+
+-- 스포티 스타일
+(7, 3),    -- 후드티
+(10, 3),   -- 나시
+(14, 3),   -- 반바지
+(16, 3),   -- 트레이닝 팬츠
+(17, 3),   -- 조거 팬츠
+(24, 3),   -- 후드 집업
+(31, 3),   -- 운동화
+(39, 3),   -- 스니커즈
+(40, 3);   -- 크록스
+
+INSERT INTO situation (situation_name) VALUES
+                                           ('daily'),    -- 1: 일상
+                                           ('travel'),   -- 2: 여행
+                                           ('exercise'), -- 3: 운동
+                                           ('couple'),   -- 4: 데이트
+                                           ('formal');   -- 5: 격식있는 자리
+
+INSERT INTO outfit_situation (outfit_seq, situation_seq) VALUES
+-- 일상 상황에 적합한 복장
+(1, 1),   -- 맨투맨
+(2, 1),   -- 반팔 티셔츠
+(4, 1),   -- 니트 스웨터
+(5, 1),   -- 후드티
+(6, 1),   -- 폴로 반팔티
+(10, 1),  -- 터틀넥 스웨터
+(12, 1),  -- 긴청바지
+(13, 1),  -- 긴면바지
+(14, 1),  -- 반바지
+(16, 1),  -- 트레이닝 팬츠
+(24, 1),  -- 후드 집업
+(31, 1),  -- 운동화
+(32, 1),  -- 슬리퍼
+(39, 1),  -- 스니커즈
+
+-- 여행 상황에 적합한 복장
+(1, 2),   -- 맨투맨
+(3, 2),   -- 셔츠
+(5, 2),   -- 후드티
+(8, 2),   -- 터틀넥 스웨터
+(12, 2),  -- 긴청바지
+(14, 2),  -- 반바지
+(20, 2),  -- 자켓
+(28, 2),  -- 바람막이
+(31, 2),  -- 운동화
+(39, 2),  -- 스니커즈
+
+-- 운동 상황에 적합한 복장
+(7, 3),   -- 후드티
+(10, 3),  -- 나시
+(14, 3),  -- 반바지
+(16, 3),  -- 트레이닝 팬츠
+(17, 3),  -- 조거 팬츠
+(24, 3),  -- 후드 집업
+(31, 3),  -- 운동화
+(39, 3),  -- 스니커즈
+(40, 3),  -- 크록스
+
+-- 데이트 상황에 적합한 복장
+(3, 4),   -- 셔츠
+(5, 4),   -- 반팔셔츠
+(6, 4),   -- 린넨셔츠
+(15, 4),  -- 슬랙스
+(22, 4),  -- 코트
+(26, 4),  -- 트렌치코트
+(35, 4),  -- 로퍼
+(38, 4),  -- 하이힐
+
+-- 격식있는 자리 상황에 적합한 복장
+(3, 5),   -- 셔츠
+(5, 5),   -- 반팔셔츠
+(8, 5),   -- 터틀넥 스웨터
+(15, 5),  -- 슬랙스
+(21, 5),  -- 자켓
+(22, 5),  -- 코트
+(26, 5),  -- 트렌치코트
+(35, 5),  -- 로퍼
+(37, 5),  -- 플랫슈즈
+(38, 5);  -- 하이힐
