a3ace6819da7cc0b44597ed2c44a069849273d8c
SANGHYUN0519 <tkdgus0519@naver.com>
2024-11-04 19:12:41 +0900

    chore : #62 마이페이지 닉네임도 수정 가능

--- article1_be/article1-be/src/main/java/article1be/admin/service/AdminService.java
+++ article1_be/article1-be/src/main/java/article1be/admin/service/AdminService.java
@@ -52,12 +52,15 @@ public class AdminService {
         );
     }
 
-    // 회원 상태 변경
-    public void updateUserStatus(AdminDTO.UserStatusUpdateRequest statusUpdate) {
-        User user = adminRepository.findById(statusUpdate.getUserSeq())
+    // 회원 상태 및 닉네임 변경
+    public void updateUser(AdminDTO.UserUpdateRequest updateRequest) {
+        User user = adminRepository.findById(updateRequest.getUserSeq())
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
 
-        user.setUserState(statusUpdate.getUserState());
+        // 닉네임과 상태는 항상 필수 값으로 설정
+        user.setUserNickname(updateRequest.getUserNickname());
+        user.setUserState(updateRequest.getUserState());
+
         adminRepository.save(user);
     }
 }
