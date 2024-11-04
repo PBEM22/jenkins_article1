a8bc5b3d25fa86ba7fe39975efa2b0bb0cfe809a
PBEM22 <dlacofbs1229@gmail.com>
2024-11-04 15:47:32 +0900

    feat: #59 메인페이지 작성, 라우터 변형 및 추가

---
+++ article1_fe/article1-fe/src/views/main/MainView.vue
@@ -0,0 +1,66 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="main">
+
+    <div class="main-content">
+
+      <div class="main-title">
+        <h1>입기 좋은 날</h1>
+      </div>
+
+      <div class="main-sub_title">
+        <h3>날씨와 대기상황에 맞게 옷을 골라보세요!</h3>
+      </div>
+
+      <div class="main-move_bar">
+        <span class="more-content">더 알아보기</span>
+        <div class="under">
+          <img src="https://img.icons8.com/?size=100&id=26195&format=png&color=000000" width="30px">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .main{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-image: url('@/assets/images/back-main.png');
+    background-position: center center;
+    text-align: center;
+  }
+  h1{
+    margin-top: 0;
+    font-weight: bold;
+    font-size: 2.5em;
+    margin-bottom: 5%;
+  }
+  .main-sub_title{
+    margin-bottom: 10%;
+  }
+  .main-content{
+    background-color: rgb(255, 255, 255, 0.5);
+    width: 85%;
+    height: 80%;
+    border-radius: 50px;
+    position: relative;
+    align-content: center;
+  }
+  .main-move_bar{
+    width: 100%;
+    height: 15%;
+    position: absolute;
+    bottom: 0;
+    background: linear-gradient(rgba(100, 156, 203, 0.4), rgba(0, 0, 0, 0.4));
+    border-radius: 0 0 50px 50px;
+    align-content: center;
+  }
+</style>
