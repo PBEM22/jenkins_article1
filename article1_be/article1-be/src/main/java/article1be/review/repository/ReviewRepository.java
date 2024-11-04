c07a95f2e78e6bb49db8f74099f00e86e792a7ac
SANGHYUN0519 <tkdgus0519@naver.com>
2024-11-04 09:09:41 +0900

    chore : #27 필요없는 부분 삭제

--- article1_be/article1-be/src/main/java/article1be/review/repository/ReviewRepository.java
+++ article1_be/article1-be/src/main/java/article1be/review/repository/ReviewRepository.java
@@ -13,8 +13,4 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     // 특정 유저가 작성한 리뷰 조회
     List<Review> findByUserSeq(Long userSeq);
 
-    // 특정 선택 기록(selectSeq)에 대한 리뷰 조회
-    List<Review> findBySelectSeq(Long selectSeq);
-
-
 }
