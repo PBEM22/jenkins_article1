06be8f41a659c19b2cff820c5ac563b1c8aa8892
Pangtaek <zombie3632@gmail.com>
2024-11-04 14:26:37 +0900

    fix: 게시판 반환 정보 수정

--- article1_be/article1-be/src/main/java/article1be/amazonS3/service/AmazonS3Service.java
+++ article1_be/article1-be/src/main/java/article1be/amazonS3/service/AmazonS3Service.java
@@ -2,6 +2,8 @@ package article1be.amazonS3.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -23,26 +25,40 @@ public class AmazonS3Service {
         this.client = client;
     }
 
-    public String upload(MultipartFile image) throws IOException {
+    public MetaData upload(MultipartFile image) throws IOException {
+        MetaData metaData = new MetaData();
+
         /* 업로드할 파일의 이름을 변경 */
         String originalFileName = image.getOriginalFilename();
+        metaData.setOriginalFileName(originalFileName);
         String fileName = changeFileName(originalFileName);
+        metaData.setChangeFileName(changeFileName(originalFileName));
 
         /* S3에 업로드할 파일의 메타데이터 생성 */
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType(image.getContentType());
+        metaData.setType(image.getContentType());
         metadata.setContentLength(image.getSize());
 
         /* S3에 파일 업로드 */
         client.putObject(amazonS3Bucket, fileName, image.getInputStream(), metadata);
 
         /* 업로드한 파일의 S3 URL 주소 반환 */
-        return client.getUrl(amazonS3Bucket, fileName).toString();
+        return metaData;
     }
 
     private String changeFileName(String originalFileName) {
         /* 업로드할 파일의 이름을 변경하는 로직 */
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
         return originalFileName + "_" + LocalDateTime.now().format(formatter);
+    }
+
+    @Getter
+    @Setter
+    public static class MetaData {
+        private String originalFileName;
+        private String changeFileName;
+        private String url;
+        private String type;
     }
 }
