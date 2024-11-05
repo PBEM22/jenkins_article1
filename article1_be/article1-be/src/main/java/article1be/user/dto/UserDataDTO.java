bc8354f42f4a352868329a51e038db13a960260b
SANGHYUN0519 <tkdgus0519@naver.com>
2024-11-05 15:45:20 +0900

    chore : #76 유저 타입 수정

--- article1_be/article1-be/src/main/java/article1be/user/dto/UserDataDTO.java
+++ article1_be/article1-be/src/main/java/article1be/user/dto/UserDataDTO.java
@@ -1,5 +1,7 @@
 package article1be.user.dto;
 
+import article1be.user.entity.UserCondition;
+import article1be.user.entity.UserStyle;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,14 +10,17 @@ import lombok.Data;
 @Data
 public class UserDataDTO {
 
+    @NotNull
+    private Long userSeq;
+
     @NotBlank(message = "닉네임은 필수 입력값입니다.")
     @Size(min = 1, max = 15, message = "닉네임은 1자 이상, 15자 이하이어야 합니다.")
     private String userNickname;
 
     @NotNull(message = "스타일은 필수 입력값입니다.")
-    private Long styleSeq;
+    private UserStyle styleSeq;
 
     @NotNull(message = "체질은 필수 입력값입니다.")
-    private Long conditionSeq;
+    private UserCondition conditionSeq;
 
 }
