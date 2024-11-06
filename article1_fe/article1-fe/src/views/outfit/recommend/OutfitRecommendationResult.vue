8192c0617b3683887ed56d092e6dc3d88366a520
person76 <94957124+person76@users.noreply.github.com>
2024-11-07 01:46:08 +0900

    [FEAT] 회원, 비회원 복장 추천 화면 구현

---
+++ article1_fe/article1-fe/src/views/outfit/recommend/OutfitRecommendationResult.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <GuestOutfitRecommendation />
+  </div>
+</template>
+
+<script>
+import GuestOutfitRecommendation from '@/components/outfit/recommend/GuestOutfitRecommendation.vue';
+
+export default {
+  components: {
+    GuestOutfitRecommendation,
+  },
+};
+</script>
