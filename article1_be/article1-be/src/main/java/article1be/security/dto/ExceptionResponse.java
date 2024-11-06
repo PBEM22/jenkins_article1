e78cf2ad94d9e72a79b23483d2b88a791d3f0695
ygc1994 <ygc1994@naver.com>
2024-11-06 14:41:17 +0900

    feat: #9 Security 관련 기능 구현

---
+++ article1_be/article1-be/src/main/java/article1be/security/dto/ExceptionResponse.java
@@ -0,0 +1,18 @@
+package article1be.security.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionResponse {
+
+    private int statusCode;
+    private String message;
+
+    public ExceptionResponse(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+}
