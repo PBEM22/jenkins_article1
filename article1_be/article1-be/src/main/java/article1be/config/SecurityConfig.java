5ecfa41fc5e5653698c0f2d3ef44e6cb945b4249
ygc1994 <ygc1994@naver.com>
2024-11-06 16:59:26 +0900

    refactor: #9 Security 관련 기능 수정

--- article1_be/article1-be/src/main/java/article1be/config/SecurityConfig.java
+++ article1_be/article1-be/src/main/java/article1be/config/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록
@@ -35,6 +34,12 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                                .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/swagger-resources/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/swagger-ui/index.html")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/webjars/**")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/login/**")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/weather/**")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/guest/**", "GET")).permitAll()
@@ -46,7 +51,6 @@ public class SecurityConfig {
                         .userInfoEndpoint(user -> user.userService(socialLoginService))
                         .successHandler(new OAuth2LoginSuccessHandler(env)) // JWT 발행 로직이 포함된 핸들러
                         .failureHandler(new OAuth2LoginFailureHandler()) // JWT 발행 로직이 포함된 핸들러
-                        .defaultSuccessUrl("/")
                 ).sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
@@ -69,16 +73,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CorsFilter corsFilter() {
-        return new CorsFilter(corsConfigurationSource());
-    }
-
-    @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:8080"); // 허용할 도메인
+        config.addAllowedOrigin("http://localhost:5173"); // 허용할 도메인
         config.addAllowedHeader("*"); // 모든 헤더 허용
         config.addAllowedMethod("*"); // 모든 HTTP 메소드 허용
         config.addExposedHeader("token"); // 서버 측에서 보내는 헤더에 대한 허용 설정
