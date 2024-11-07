c7d810da1ad10cba067ff9e2ade9cf94d9017f24
PBEM22 <dlacofbs1229@gmail.com>
2024-11-08 02:14:23 +0900

    feat: #131 pinia 수정 및 로그인시 로컬스토리지 저장, 로그인 성공 핸들러 헤더아닌 쿠키로 반환하게 설정

--- article1_fe/article1-fe/src/store/authStore.js
+++ article1_fe/article1-fe/src/store/authStore.js
@@ -4,27 +4,29 @@ import {onMounted, ref} from "vue";
 export const useAuthStore = defineStore('auth', () => {
     const accessToken = ref(null);
     const userRole = ref(null);
+    const isLoggedIn = ref(false);
 
     onMounted(() => {
         const token = localStorage.getItem('accessToken');
-        console.log(token);
         if (token) {
             accessToken.value = token;
-            // accessToken.value = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjQiLCJhdXRoIjpbIlVTRVIiXSwiZXhwIjoxNzMwOTY3NTc5fQ.WM-J5dn4JxLlq4-POsbWEDNut6XAZeK50y9U2pxkIM5_oATWiqL2NaeFQwr7kh3B2kVbSw_-mcDWJJ17hIqGUQ";
-            const payload = JSON.parse(atob(token.split('.')[1])); // JWT 토큰의 페이로드 추출
+            const payload = JSON.parse(atob(token.split('.')[1]));
             userRole.value = payload.auth[0].slice(5);
-            console.log(userRole.value)
+            isLoggedIn.value = true;
         }
     });
 
     function login(token) {
+        isLoggedIn.value = true;
         accessToken.value = token;
         localStorage.setItem('accessToken', token);
         const payload = JSON.parse(atob(token.split('.')[1]));
         userRole.value = payload.auth[0].slice(5);
+        console.log(userRole);
     }
 
     function logout() {
+        isLoggedIn.value = false;
         accessToken.value = null;
         userRole.value = null;
         localStorage.removeItem('accessToken');
@@ -35,5 +37,5 @@ export const useAuthStore = defineStore('auth', () => {
         return userRole.value.includes(requiredRole);
     }
 
-    return {accessToken, userRole, login, logout, isAuthorized};
+    return {accessToken, userRole, login, logout, isAuthorized, isLoggedIn};
 });
