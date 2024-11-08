042775db2f3e856cd2cc63c3603fa308bf56da57
Pangtaek <zombie3632@gmail.com>
2024-11-08 09:45:50 +0900

    feat: 신고 라우터 개발

---
+++ article1_fe/article1-fe/src/views/blame/BlameList.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+
+import Container from "@/components/board/Container.vue";
+import TitleBar from "@/components/board/TitleBar.vue";
+</script>
+
+<template>
+  <Container>
+    <TitleBar reg-data="2022-10-21"
+  </Container>
+
+</template>
+
+<style scoped>
+
+</style>
