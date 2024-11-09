5b452f0379a8e3ae5af187accbc04973fcf8c2f6
JIYOUNG-22 <dyong0922@naver.com>
2024-11-09 21:44:23 +0900

    feat: 정지/탈퇴한 회원 로그인 시 에러페이징 처리

--- article1_fe/article1-fe/src/router/userRouter.js
+++ article1_fe/article1-fe/src/router/userRouter.js
@@ -1,5 +1,7 @@
 import UserLogin from "@/views/user/UserLogin.vue";
 import UserData from "@/views/user/UserData.vue";
+import DeletedUserError from "@/views/user/DeletedUserError.vue";
+import BannedUserError from "@/views/user/BannedUserError.vue";
 
 export default [
     {
@@ -11,5 +13,15 @@ export default [
         // 첫 로그인(회원가입) 시 추가정보 등록 화면
         path: '/user/data',
         component: UserData
+    },
+    {
+        // 탈퇴한 회원이 로그인 요청 시
+        path: '/user/delete',
+        component: DeletedUserError
+    },
+    {
+        // 정지 당한 회원이 로그인 요청 시
+        path: '/user/ban',
+        component: BannedUserError
     }
 ];
