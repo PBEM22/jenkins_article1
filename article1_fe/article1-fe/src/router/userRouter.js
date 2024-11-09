f7bcabe1f809bee2763cee03bca0fe25dad70cef
JIYOUNG-22 <dyong0922@naver.com>
2024-11-09 15:38:21 +0900

    feat: 회원가입 시 추가정보(닉네임, 선호도) 등록 기능

--- article1_fe/article1-fe/src/router/userRouter.js
+++ article1_fe/article1-fe/src/router/userRouter.js
@@ -1,4 +1,5 @@
 import UserLogin from "@/views/user/UserLogin.vue";
+import UserData from "@/views/user/UserData.vue";
 
 export default [
     {
@@ -6,5 +7,9 @@ export default [
         path: '/login',
         component: UserLogin
     },
-
+    {
+        // 첫 로그인(회원가입) 시 추가정보 등록 화면
+        path: '/user/data',
+        component: UserData
+    }
 ];
